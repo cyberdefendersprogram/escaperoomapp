eb33a6ebea8e2f0b417d89eb5bf9322e10cbd8eb
Vaibhav Bhandari <vaibhavb@gmail.com>
2019-10-26 15:15:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
-app.use(cors);
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
